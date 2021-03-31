a0b20787f802cfbd5de8774554e65f573ed0238a
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-31 14:41:46 -0500

    Add create action

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -8,4 +8,10 @@ class BulletinsController < ApplicationController
         bulletin = Bulletin.find_by(id: params[:id])
         render json: bulletin, include: [:boards]
     end
+
+    def create 
+        binding.pry
+        bulletin = Bulletin.create(params[:bulletin])
+        render json: bulletin
+    end
 end
