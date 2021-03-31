5b78dc15a8944fefdddcba8bb7451c42eb87d807
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-31 14:18:42 -0500

    Middleware to deal with corrs

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -1,2 +1,11 @@
 class BulletinsController < ApplicationController
+    def index 
+        bulletins = Bulletin.all
+        render json: bulletins  
+    end
+
+    def show 
+        bulletin = Bulletin.find_by(id: params[:id])
+        render json: bulletin, include: [:boards]
+    end
 end
