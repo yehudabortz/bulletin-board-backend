4c1c81497b0273b2511dade329b724e9fed01f03
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-02 14:29:54 -0500

    Create new Items associated with board

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+
+    def create
+        item = Item.create(item_params)
+        render json: item
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:title, :body, :board_id)
+    end
 end
