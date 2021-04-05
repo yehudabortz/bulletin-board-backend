9ffac2c5724c562fcdee6a9ccf5050fcd3c069a3
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-05 12:01:02 -0500

    Add destroy action for bulletins

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -14,8 +14,17 @@ class BulletinsController < ApplicationController
         bulletin = Bulletin.create(bulletin_params)
         render json: bulletin
     end
+    
+    def destroy 
+        bulletin = Bulletin.find_by(id: params[:id])
+        bulletin.destroy
+        render json: bulletin
+    end
+
+    private
 
     def bulletin_params
         params.require(:bulletin).permit(:name)
     end
+
 end
